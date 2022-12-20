fe57260d0f7e0e220e39fe0cf4573039d409fbc1
cecep29 <cecepefendi.c2p@gmail.com>
2022-12-20 17:33:32 +0700

    init project

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Expenses App",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Personal Expenses"),
+      ),
+      body: const Center(
+        child: Text("Widget Playground"),
+      ),
+    );
+  }
+}
